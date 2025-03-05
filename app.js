59143a5fd886e45dbcf65b366c7969c9617da6a6
sumavarma <144315644+sumavarma@users.noreply.github.com>
2025-03-05 18:56:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello, World Everyone!\n');
+  res.end('Hello, World Miss Beauty!\n');
 });
 
 server.listen(3000, () => {
